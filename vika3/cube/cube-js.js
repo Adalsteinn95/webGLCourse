f4a51bbca5a9d2f5d634be20c1a4d5576c84014c
adalsteinn95 <adalsteinn95@gmail.com>
2018-02-06 17:01:38 +0000

    checkpoint

---
+++ vika3/cube/cube-js.js
@@ -0,0 +1,214 @@
+/////////////////////////////////////////////////////////////////
+//    S�nid�mi � T�lvugraf�k
+//     V�rpunarfylki b�i� til � JS og sent yfir til
+//     hn�talitara, sem margfaldar (�.e. varpar)
+//
+//    Hj�lmt�r Hafsteinsson, febr�ar 2018
+/////////////////////////////////////////////////////////////////
+var canvas;
+var gl;
+
+var NumVertices  = 36;
+
+var points = [];
+var colors = [];
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+
+var axis = 0;
+var theta = [ 0, 0, 0 ];
+
+var movement = false;     // Do we rotate?
+var spinX = 0;
+var spinY = 0;
+
+var lastX = 0;
+var lastY = 0; 
+
+var origX;
+var origY;
+
+xDirection = 0;
+
+
+/* changeSize */
+var size = false;
+var changeSize = 1;
+
+var matrixLoc;
+
+var lastScrollTop = 0;
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    colorCube();
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    matrixLoc = gl.getUniformLocation( program, "rotation" );
+
+    //event listeners for mouse
+    canvas.addEventListener("mousedown", function(e){
+        movement = true;
+        origX = e.offsetX;
+        origY = e.offsetY;
+
+        e.preventDefault();         // Disable drag and drop
+    } );
+
+    canvas.addEventListener("mouseup", function(e){
+        movement = false;
+        size = false;
+
+    } );
+
+    window.addEventListener("keydown", function(e){
+        if(e.key === "Shift"){
+            size = true;
+        }
+
+        if(e.key === "ArrowLeft"){
+            xDirection -= 0.05;
+        }
+
+        if(e.key === "ArrowRight"){
+            xDirection += 0.05;
+        }
+    } );
+
+    canvas.addEventListener("mousemove", function(e){
+        if(movement) {
+    	    spinY = ( spinY + (e.offsetX - origX) ) % 360;
+            spinX = ( spinX + (e.offsetY - origY) ) % 360;
+            origX = e.offsetX;
+            origY = e.offsetY;
+
+            if(size){
+                if(spinX < lastX){
+                    changeSize -= 0.02;
+                } else {
+                    changeSize += 0.02;
+                }
+                 
+            }
+
+            lastX = spinX;
+            lastY = spinY;
+            
+            
+
+        }
+
+    } );
+    
+    render();
+}
+
+function colorCube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+function quad(a, b, c, d) 
+{
+    var vertices = [
+        vec3( -0.5, -0.5,  0.5 ),
+        vec3( -0.5,  0.5,  0.5 ),
+        vec3(  0.5,  0.5,  0.5 ),
+        vec3(  0.5, -0.5,  0.5 ),
+        vec3( -0.5, -0.5, -0.5 ),
+        vec3( -0.5,  0.5, -0.5 ),
+        vec3(  0.5,  0.5, -0.5 ),
+        vec3(  0.5, -0.5, -0.5 )
+    ];
+
+    var vertexColors = [
+        [ 0.0, 0.0, 0.0, 1.0 ],  // black
+        [ 1.0, 0.0, 0.0, 1.0 ],  // red
+        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
+        [ 0.0, 1.0, 0.0, 1.0 ],  // green
+        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
+        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
+        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
+        [ 1.0, 1.0, 1.0, 1.0 ]   // white
+    ];
+
+    // We need to partition the quad into two triangles in order for
+    // WebGL to be able to render it.  In this case, we create two
+    // triangles from the quad indices
+    
+    //vertex color assigned by the index of the vertex
+    
+    var indices = [ a, b, c, a, c, d ];
+
+    for ( var i = 0; i < indices.length; ++i ) {
+        points.push( vertices[indices[i]] );
+        //colors.push( vertexColors[indices[i]] );
+    
+        // for solid colored faces use 
+        colors.push(vertexColors[a]);
+        
+    }
+}
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var ctm = mat4();
+
+    /*
+    ctm = mult( ctm, rotateX(spinX) );
+    ctm = mult( ctm, rotateY(spinY) );
+    */
+
+    ctm = mult( ctm, translate(xDirection,0,0) );
+    ctm = mult( ctm, rotateX(spinX) );
+    ctm = mult( ctm, rotateY(spinY) );
+    ctm = mult( ctm, scalem(changeSize,changeSize,changeSize) );
+
+
+
+    gl.uniformMatrix4fv(matrixLoc, false, flatten(ctm));
+
+    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+
+    requestAnimFrame( render );
+}
+
